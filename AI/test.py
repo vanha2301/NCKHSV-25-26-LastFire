e5186f06ad87b644e43a0be39b8aeff9aff37fc7
vanha2301 <vanha.cv.vn@gmail.com>
2026-01-28 22:56:10 +0700

    add and delete

--- AI/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-csv_path = "NASA/DL_FIRE_SV-C2_684272/fire_archive_SV-C2_684272.csv"  # đổi path nếu cần
-
-df = pd.read_csv(csv_path, dtype=str)  # đọc dạng chuỗi để giữ nguyên format
-out = df[["longitude", "latitude", "acq_date"]].copy()
-
-print(out['acq_date'].iloc[2])
--- AI/testFileCsv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-rows = [
-    {"longitude": 108.25682, "latitude": 12.42611, "date": "2012-02-08"},
-    {"longitude": 108.25694, "latitude": 12.42818, "date": "2012-02-08"},
-]
-
-df = pd.DataFrame(rows)
-df.to_csv("lon_lat_date.csv", index=False, encoding="utf-8-sig")
